149079d7109a4b9a2a813aa65a3cc29bf2c046db
Steven Lundy <steven.lundy@gmail.com>
2015-10-16 15:10:35 -0700

    Write tests for experiment api

--- specs/serverspecs/expspecs/routeSpecs.js
+++ specs/serverspecs/expspecs/routeSpecs.js
@@ -1,32 +1,117 @@
 'use strict';
 
 var expect = require('chai').expect;
+var request = require('supertest');
+var app = require('../../../server/index.js');
 var sinon = require('sinon');
 
 describe('The Experiment Router', function () {
+  var exampleExp = require('../../../server/exps/example.js');
+  var exampleUser = require('../../../server/users/example.js');
+  var userId;
+  
   beforeEach(function(){
-
+    request(app)
+      .post('/users/')
+      .send(exampleUser)
+      .end(function(err, res) {
+        userId = res.body._id;
+      });
   });
 
   afterEach(function(){
+    request(app)
+      .delete('/users/'+userId);
+  });
+
+  it('responds to POST requests at `/users/:userId/experiments/`', function (done) {
+
+    request(app)
+      .post('/users/'+userId+'/experiments/')
+      .send(exampleExp)
+      .expect(200)
+      .expect(function(res){
+        if(res.body.name === exampleExp.name) {
+          return done();
+        }
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
+      });
+  });
+
+  it('responds to GET requests at `/users/:userId/experiments/`', function (done) {
+    request(app)
+      .get('/users/'+userId+'/experiments/')
+      .expect(200)
+      .expect(function(res){
+        if(Array.isArray(res.body)){
+          return done();
+        }
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
+      });
 
   });
 
-  it('should call ctrl.getAllExps on GET /user/:userId/experiments/', function () {
-  
+  it('returns previously created experiments on GET requests at `/users/:userId/experiments/`', function (done) {
+    request(app)
+      .post('/users/'+userId+'/experiments/')
+      .send(exampleExp)
+      .end(function(err, res){
+        request(app)
+          .get('/users/'+userId+'/experiments/')
+          .expect(200)
+          .expect(function(res){
+            if(res.body.length === 1){
+              return done();
+            } else {
+              throw 'No experiments';
+            }
+          })
+          .end(function (err, res) {
+            if (err) return done(err);
+          });
+      });
+
   });
 
-  it('should call ctrl.addExp on POST /user/:userId/experiments/', function () {
-  
+  it('responds to DELETE requests at `/users/:userId/experiments/:expId`', function (done) {
+    var expId;
+
+    request(app)
+      .post('/users/'+userId+'/experiments/')
+      .send(exampleExp)
+      .end(function(err, res) {
+        expId = res.body._id;
+        request(app)
+          .delete('/users/'+userId+'/experiments/'+expId)
+          .expect(200)
+          .expect(function(res){
+            if(res.body.name === exampleExp.name) {
+              return done();
+            }
+          })
+          .end(function (err, res) {
+            if (err) return done(err);
+          });
+      });
   });
 
-  it('should call ctrl.deleteExp on DELETE /user/:userId/experiments/:expId', function () {
-  
-  });
+  it('responds to GET requests at `/users/:userId/experiments/:expId`', function (done) {
+    var expId;
 
-  it('should call ctrl.getExp on GET /user/:userId/experiments/:expId', function () {
-  
-  });
+    request(app)
+      .post('/users/'+userId+'/experiments/')
+      .send(exampleExp)
+      .end(function(err, res) {
+        expId = res.body._id;
+        request(app)
+          .get('/users/'+userId+'/experiments/'+expId)
+          .expect(200, 'removed exp', done);
+      });
 
+  });
 
 });
