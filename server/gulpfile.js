181f06fa0273a4032fad7761d02954de01a1dc00
Marcus Buffett <marcusbuffett@me.com>
2015-10-16 11:59:16 -0700

    Update dependencies and setup gulpfile bundling

---
+++ server/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var mocha = require('gulp-mocha');
+var through2 = require('through2');
+// var watchify = r
+
+gulp.task('buildJS', function () {
+  // return bundler.bundle()
+          // .pipe(gulp.dest('./client/public/scripts'));
+  return gulp
+    .src('./client/index.js')
+    .pipe(through2.obj(function(file, enc, next) {
+      browserify(file.path, {debug : true})
+        .transform(babelify)
+        .bundle(function(err, res) {
+          if (err) {
+            return next(err);
+          }
+          file.contents = res;
+          next(null, file);
+        });
+    }))
+    .pipe(gulp.dest('./client/public/scripts'));
+});
