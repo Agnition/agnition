831c0cce5a0b6f24158bf4c3eaf85471c7c6e59b
Steven Lundy <steven.lundy@gmail.com>
2015-10-16 12:54:37 -0700

    (test) Write passing User api tests

--- specs/serverspecs/userspecs/routeSpecs.js
+++ specs/serverspecs/userspecs/routeSpecs.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var expect = require('chai').expect;
+var request = require('supertest');
+var app = require('../../../server/index.js');
 var sinon = require('sinon');
 
 describe('The User Router', function () {
+
   beforeEach(function(){
 
   });
@@ -12,17 +15,69 @@ describe('The User Router', function () {
 
   });
 
-  it('should call ctrl.getUser on GET /user/:ID', function () {
-  
+  it('responds to POST requests at `/users/`', function (done) {
+    var data = {
+      username: 'johnsmith'
+    };
+
+    request(app)
+      .post('/users/')
+      .send(data)
+      .expect(200)
+      .expect(function(res){
+        if(res.body.username === 'johnsmith') {
+          return done();
+        }
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
+      });
   });
 
-  it('should call ctrl.addUser on POST /user/', function () {
-  
+  it('responds to GET requests at `/users/:ID`', function (done) {
+    var userId;
+
+    var data = {
+      username: 'johnsmith'
+    };
+
+    request(app)
+      .post('/users/')
+      .send(data)
+      .end(function(err, res) {
+        userId = res.body._id;
+        request(app)
+          .get('/users/'+userId)
+          .expect(function(res){
+            res.body.exps = '';
+            res.body.__v = 0;
+          })
+          .expect(200, {
+            __v: 0,
+            _id: userId,
+            username: 'johnsmith',
+            exps: ''
+          }, done);
+      });
+    
   });
 
-  it('should call ctrl.deleteUser on DELETE /user/:ID', function () {
-  
-  });
+  it('responds to DELETE requests at `/users/:ID`', function (done) {
+    var userId;
 
+    var data = {
+      username: 'johnsmith'
+    };
+
+    request(app)
+      .post('/users/')
+      .send(data)
+      .end(function(err, res) {
+        userId = res.body._id;
+        request(app)
+          .delete('/users/'+userId)
+          .expect(200, 'removed user', done);
+      });
+  });
 
 });
