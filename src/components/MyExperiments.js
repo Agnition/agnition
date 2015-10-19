fdd208aafb732c3e654b6ac494f09e1efae7f36e
hdh3000 <henry.hutcheson@gmail.com>
2015-10-19 11:27:17 -0700

    commiting before rebasing

--- src/components/MyExperiments.js
+++ src/components/MyExperiments.js
@@ -1,19 +1,21 @@
 var React = require('react');
-var About = require('./About.jsx');
-var Signin = require('./Signin.jsx');
-var Hypothesis = require('./Hypothesis');
 var OpenExps = require('./OpenExps');
+var ClosedExps = require('./ClosedExps');
 
 // STATELOGIC logic to add in state properties
 // STATELOGIC create the my exps variable
+// STATELOGIC loadDB here...
 
 var MyExps = React.createClass({
   render: function() {
     return (
       <div>
       //STATELOGIC ROUTING how to pass in the correct set of exps?
-      //currently setup to just filter my expierments...
+      //currently setup to just filter my expierments inside open and closed exp components...
+      <h2>Open Expierments</h2>
       <OpenExps myExperiments = {myExperiments} />
+      
+      <h2>Closed Expierments</h2>
       <ClosedExps myExperiments = {myExperiments} />
 
       </div>
