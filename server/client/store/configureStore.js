75184c7f37812989edadd1d44c0c3ae018aee643
Matthew Brooks <mdeanbrooks@gmail.com>
2015-10-19 20:32:50 -0700

    keep config store the same for now

--- server/client/store/configureStore.js
+++ server/client/store/configureStore.js
@@ -1,35 +1,21 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/index.js';
-import Immutable from 'immutable';
 import { applyMiddleware, compose } from 'redux';
-// // import { reduxReactRouter } from 'redux-router';
+import { reduxReactRouter } from 'redux-router';
 import { devTools } from 'redux-devtools';
-// // import createHistory from 'history/lib/createBrowserHistory';
-// // import routes from '../routes';
+import createHistory from 'history/lib/createBrowserHistory';
+import routes from '../routes';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 
 const finalCreateStore = compose(
   applyMiddleware(thunk),
-  // reduxReactRouter({ routes, createHistory }),
+  reduxReactRouter({ routes, createHistory }),
   applyMiddleware(createLogger()),
   devTools()
 )(createStore);
 
-// var initialState = Immutable.Map({
-//   auth: false
-// });
-
 module.exports = function(initialState) {
   const store = finalCreateStore(rootReducer, initialState);
-
-  if (module.hot) {
-    module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers/index');
-
-      store.replaceReducer(nextRootReducer);
-    });
-  }
-
   return store;
 };
