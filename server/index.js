8ceae696dabd19c29e414a8bba58f3e7618cff14
Marcus Buffett <marcusbuffett@me.com>
2015-10-19 19:39:00 -0700

    Death to webpack

--- server/index.js
+++ server/index.js
@@ -2,21 +2,11 @@
 var path = require('path');
 var config = require('./config');
 
-var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var webpackConfig = require('./webpack.config');
-
 // express
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var userRouter = require('./users/routes');
-
-//webpack
-var compiler = webpack(webpackConfig);
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }));
-app.use(webpackHotMiddleware(compiler));
 
 // db
 var mongoose = require('mongoose');
@@ -28,14 +18,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes
-
-app.use(function(req, res) {
-  res.sendFile(__dirname + '/client/public/index.html');
-});
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname,'./client/public')));
 app.use('/users', userRouter);
-app.get('/', express.static(path.join(__dirname, '/index.html')));
-
 
 // start it up
 app.listen(config.port);
