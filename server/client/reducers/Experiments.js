66c9b656ac2893462e3bde512dac1f6a97545fb8
Marcus Buffett <marcusbuffett@me.com>
2015-10-19 20:20:38 -0700

    Setting state from server almost working

--- server/client/reducers/Experiments.js
+++ server/client/reducers/Experiments.js
@@ -1,16 +1,13 @@
 import Immutable from 'immutable';
-const initialState = new Immutable.List(
-  //exp object 
-
-);//.of( 1, 2, 3);
+const initialState = new Immutable.List();
 
 module.exports = function(state = initialState, action) {
-  // if (action.type === 'CHANGE_DEP_VAR') {
-  //   state.get(action.key).set('depVar').name)
+  if (action.type === 'SET_EXPERIMENTS') {
+    console.log("SETTING STATE!");
 
-  //   return state.set('user', action.user);
+    return Immutable.fromJS(action.experiments.result.exps);
 
-  // } else {
+  } else {
     return state;
-  // }
+  }
 };
