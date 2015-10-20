3a596d751fa81f033964fccf083556eddda3f441
hdh3000 <henry.hutcheson@gmail.com>
2015-10-19 20:51:30 -0700

    commiting before rebasing

--- server/client/components/Signin.js
+++ server/client/components/Signin.js
@@ -1,40 +1,46 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var $ = require('jquery');
+
 var ExperimentActions = require('../actions/Experiments.js');
+// var ExperimentActions = require('../actions/Experiments.js');
+// var ExperimentActions = require('../actions/Experiments.js');
+// var ExperimentActions = require('../actions/Experiments.js');
+// var ExperimentActions = require('../actions/Experiments.js');
+// var ExperimentActions = require('../actions/Experiments.js');
+
 var UserActions = require('../actions/Users.js');
-var normalize = require('../utils/normalize')
+var normalize = require('../utils/normalize');
 var connect = require('react-redux').connect;
 var bindActionCreators = require('redux').bindActionCreators;
 
 
 function mapStatetoProps (state) {
   return {
-    // hypothesis: state.Hypothesis.get('hypothesis'),
-    // iv: state.Hypothesis.get('iv'),
-    // dv: state.Hypothesis.get('dv')
   };
 }
 
 function mapDispatchtoProps (dispatch) {
   return {
     actions: bindActionCreators(UserActions, dispatch),
-    expActions: bindActionCreators(ExperimentActions, dispatch)
+    expActions: bindActionCreators(ExperimentActions, dispatch),
+    // depVarActions: bindActionCreators(DependentVarActions, dispatch),
+    // indVarActions: bindActionCreators(IndependentVarActions, dispatch),
+    // mesActions: bindActionCreators(MeasureActions, dispatch),
+    // reqActions: bindActionCreators(RequestActions, dispatch),
+    // remActions: bindActionCreators(ReminderActions, dispatch),
   };
 }
 
 var Signin = React.createClass({
   handleClick: function() {
-    $.get('/users/562586f131b59bc2557187f3/experiments', function(data) {
+    // need to set this to be a dynamic user id-- will break on you in dev
+    $.get('/users/5625837d9d64345698bfa700/experiments', function(data) {
       //normalize
       console.log("===========================================");
-      console.log(data);
-      var n = normalize({
-        exps : data
-      });
-      console.log(n);
-      console.log(this.props.expActions);
-      this.props.expActions.setExperiments(n);
+      var normData = normalize({ exps : data });
+      console.log(normData);
+      this.props.expActions.setExperiments(normData);
     }.bind(this));
   },
   render: function() {
--- server/client/reducers/Experiments.js
+++ server/client/reducers/Experiments.js
@@ -3,10 +3,7 @@ const initialState = new Immutable.List();
 
 module.exports = function(state = initialState, action) {
   if (action.type === 'SET_EXPERIMENTS') {
-    console.log("SETTING STATE!");
-
-    return Immutable.fromJS(action.experiments.result.exps);
-
+    return Immutable.fromJS(action.experiments.entities.experiments);
   } else {
     return state;
   }
