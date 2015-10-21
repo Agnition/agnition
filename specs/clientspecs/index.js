d3485b4f091788d7aa7ddd7747c8883243acfadb
Matthew Brooks <mdeanbrooks@gmail.com>
2015-10-21 16:17:12 -0700

    Fix front end testing for connected components

--- specs/clientspecs/index.js
+++ specs/clientspecs/index.js
@@ -1,30 +1,33 @@
 'use strict';
-var jsdom = require('jsdom');
+module.exports = function () {
 
-// setup the simplest document possible
-var doc = jsdom.jsdom('<!doctype html><html><body><div id="root"></div></body></html>');
+  var jsdom = require('jsdom');
 
-// get the window object out of the document
-var win = doc.defaultView;
+  // setup the simplest document possible
+  var doc = jsdom.jsdom('<!doctype html><html><body><div id="root"></div></body></html>');
 
-// set globals for mocha
-global.document = doc;
-global.window = win;
+  // get the window object out of the document
+  var win = doc.defaultView;
+
+  // set globals for mocha
+  global.document = doc;
+  global.window = win;
 
 
-// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
-function propagateToGlobal (window) {
-  for (var key in window) {
-    if (!window.hasOwnProperty(key)) {
-      continue;
+  // from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
+  function propagateToGlobal (window) {
+    for (var key in window) {
+      if (!window.hasOwnProperty(key)) {
+        continue;
+      }
+      if (key in global) {
+        continue;
+      }
+
+      global[key] = window[key];
     }
-    if (key in global) {
-      continue;
-    }
-
-    global[key] = window[key];
   }
-}
-// take all properties of the window object and also attach it to the
-// mocha global object
-propagateToGlobal(win);
+  // take all properties of the window object and also attach it to the
+  // mocha global object
+  propagateToGlobal(win);
+};
