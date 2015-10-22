3e13267597ab272e9c3cd3eb5ce43d414bb6609b
Matthew Brooks <mdeanbrooks@gmail.com>
2015-10-21 20:19:58 -0700

    Add changes to testOverviewSpec

--- specs/clientspecs/newExperimentOverview.spec.js
+++ specs/clientspecs/newExperimentOverview.spec.js
@@ -1,37 +1,49 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var mocha = require('mocha');
 var expect = require('chai').expect;
 var TestUtils = require('react-addons-test-utils');
 var makeDom = require('./index.js');
-var Hypothesis = require('../../server/client/containers/NewExperiment/Hypothesis.js');
+var NewExperiment = require('../../server/client/screens/NewExperiment.js');
 var sinon = require('sinon');
 var dom = makeDom();
 var Immutable = require('immutable');
 var createStore = require('redux').createStore;
+var Provider = require('react-redux').Provider;
 
 describe('New Experiment Overview Test', function () {
 
-  function createMockStore(state) {
-    return {
-      subscribe: () => {},
-      dispatch: () => {},
-      getState: () => {
-        return {...state};
-      }
-    };
-  }
-  
-  var mockStore = {};
-  mockStore.NewExperiment = Immutable.Map({
-    hypothesis: 'Route A to work is fastest',
-    cause: 'picking a better route',
-    effect: 'getting to work quickly'
+  var hypothesis;
+
+  beforeEach(function () {
+    function createMockStore(state) {
+      return {
+        subscribe: () => {},
+        dispatch: () => {},
+        getState: () => {
+          return {...state};
+        }
+      };
+    }
+
+    var mockState = {}
+      mockState.NewExperiment = Immutable.Map({
+      hypothesis: 'Route A to work is fastest',
+      cause: 'picking a better route',
+      effect: 'getting to work quickly'
+      });
+
+      hypothesis = TestUtils.renderIntoDocument(
+        <Provider store={createMockStore(mockState)}>
+        <NewExperiment />
+        </Provider>, 'root');
   });
 
-  it('Hypothesis should have hypothesis, cause, and effect props', function () {
-    var hypothesis = TestUtils.renderIntoDocument(<Hypothesis store={createMockStore(mockStore)} />, 'root');
-    expect(hypothesis.props.store.getState().NewExperiment.get('hypothesis')).to.eql('Route A to work is fastest');
-    expect(hypothesis.props.store.getState().NewExperiment.get('cause')).to.eql('picking a better route');
-    expect(hypothesis.props.store.getState().NewExperiment.get('effect')).to.eql('getting to work quickly');
+
+  it('should have hypothesis, cause, and effect props', function () {
+    var div = TestUtils.scryRenderedDOMComponentsWithTag(hypothesis,'div');
+    expect(ReactDOM.findDOMNode(div[0]).textContent).to.contain('Route A to work is fastest');
+    expect(ReactDOM.findDOMNode(div[0]).textContent).to.contain('picking a better route');
+    expect(ReactDOM.findDOMNode(div[0]).textContent).to.contain('getting to work quickly');
   });
 });
