6f37b7a3945d36c39e2da5af85671af300718d63
Marcus Buffett <marcusbuffett@me.com>
2015-10-23 10:35:50 -0700

    Un-normalize

---
+++ server/client/utils/schema.js
@@ -0,0 +1,30 @@
+var Normalize = require('normalizr');
+var Schema = Normalize.Schema;
+var arrayOf = Normalize.arrayOf;
+
+var experiment     = new Schema('experiments', {idAttribute: '_id'});
+var dependentVar   = new Schema('dependentVars', {idAttribute: '_id'});
+var independentVar = new Schema('independentVars', {idAttribute: '_id'});
+var measure        = new Schema('measures', {idAttribute: '_id'});
+var sample         = new Schema('samples', {idAttribute: '_id'});
+var reminder       = new Schema('reminders', {idAttribute: '_id'});
+var request        = new Schema('requests', {idAttribute: '_id'});
+
+experiment.define({
+  independentVars : arrayOf(independentVar),
+  dependentVars : arrayOf(dependentVar)
+});
+dependentVar.define({
+  measures : arrayOf(measure),
+  requests : arrayOf(request)
+});
+independentVar.define({
+  reminders : arrayOf(reminder)
+});
+measure.define({
+  samples : arrayOf(sample)
+});
+
+module.exports = {
+  exps: arrayOf(experiment)
+};
---
+++ server/client/utils/un-normalize.js
@@ -0,0 +1,47 @@
+var Normalize = require('normalizr');
+var Schema = Normalize.Schema;
+var arrayOf = Normalize.arrayOf;
+
+var EntitySchema = new Schema('schema').constructor;
+var ArraySchema = arrayOf({}).constructor;
+var schema = require('./schema.js');
+
+function unNormalize(obj, schema) {
+  var currentSub = {};
+  function unNormalizeSub(result, entities, schema) {
+    if (schema.constructor === ArraySchema) {
+      var populated = [];
+      for (var i = 0; i < result.length; i++) {
+        populated.push(unNormalizeSub(result[i], entities, schema._itemSchema));
+      }
+      return populated;
+    }
+    if (schema.constructor === EntitySchema) {
+      var entityKey = schema._key;
+      var populated = entities[entityKey][result];
+      for (var key in schema) {
+        if (schema.hasOwnProperty(key) &&
+          key !== '_key' &&
+          key !== '_getId' &&
+          entities.hasOwnProperty(key)) {
+            populated[key] = unNormalizeSub(populated[key], entities, schema[key]);
+          }
+      }
+      return populated;
+    }
+    for (var key in schema) {
+      if (key === '_itemSchema') {
+        return result;
+      }
+      currentSub[key] = unNormalizeSub(result[key], entities, schema[key]);
+    }
+    return currentSub;
+  }
+  var result = obj.result;
+  var entities = obj.entities;
+  return unNormalizeSub(result, entities, schema);
+}
+
+module.exports = function(data) {
+  return unNormalize(data, schema);
+};
