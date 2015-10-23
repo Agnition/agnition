146c3ecc2b341989a641236fdefc543bee3f9468
hdh3000 <henry.hutcheson@gmail.com>
2015-10-22 20:34:18 -0700

    added actions and reducers

--- server/client/actions/IndependentVars.js
+++ server/client/actions/IndependentVars.js
@@ -4,3 +4,38 @@ module.exports.setIndVars = function(indVars) {
     indVars: indVars
   };
 };
+module.exports.setName = function (name, id) {
+  return {
+    type: 'SET_NAME',
+    name : name,
+    id: id
+  };
+};
+module.exports.setActionsPerTrial = function (actionsPerTrial, id) {
+  return {
+    type: 'SET_ACTIONS_PER_TRIAL',
+    actionsPerTrial : actionsPerTrial,
+    id: id
+  };
+};
+module.exports.setNumTrials = function (numTrials, id) {
+  return {
+    type: 'SET_NUM_TRIALS',
+    numTrials : numTrials,
+    id: id
+  };
+};
+module.exports.setRandomized = function (randomized, id) {
+  return {
+    type: 'SET_RANDOMIZED',
+    randomized : randomized,
+    id: id
+  };
+};
+module.exports.pushOption = function (option, id) {
+  return {
+    type: 'PUSH_OPTION',
+    option : option,
+    id: id
+  };
+};
--- server/client/reducers/IndependentVars.js
+++ server/client/reducers/IndependentVars.js
@@ -4,6 +4,32 @@ const initialState = new Immutable.List();
 module.exports = function(state = initialState, action) {
   if (action.type === 'SET_IND_VARS') {
     return Immutable.fromJS(action.indVars);
+
+  } else if(action.type ==='SET_NAME') {
+      return state.setIn([action.indVarId],'name', action.name);
+
+  } else if(action.type ==='SET_ACTIONS_PER_TRIAL') {
+      return state.setIn([action.indVarId],'actionsPerTrial', action.actionsPerTrial);
+
+  } else if(action.type ==='SET_NUM_TRIALS') {
+      return state.setIn([action.indVarId],'numTrials', action.numTrials);
+
+  } else if(action.type ==='SET_RANDOMIZED') {
+      return state.setIn([action.indVarId],'randomized', action.randomized);
+
+  } else if(action.type ==='PUSH_OPTION') {
+      return state.setIn([action.indVarId],'option', action.option) ;
+
+  } else {
+    return state;
   }
-  return state;
 };
+
+
+
+
+
+  // state = state || initialState;
+  // if (action.type === 'SET_HYPOTHESIS') {
+  //   return state.set('hypothesis', action.hypothesis);
+  // }
