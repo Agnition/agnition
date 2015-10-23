ac9893e981b1427850768a64f6cdc7d6962327fd
Marcus Buffett <marcusbuffett@me.com>
2015-10-23 15:43:49 -0700

    Requests undefined check

--- server/client/reducers/Requests.js
+++ server/client/reducers/Requests.js
@@ -2,7 +2,7 @@ import Immutable from 'immutable';
 const initialState = new Immutable.List();
 
 module.exports = function(state = initialState, action) {
-  if (action.type === 'SET_REQUESTS') {
+  if (action.type === 'SET_REQUESTS' && action.requests) {
     return Immutable.fromJS(action.requests);
   }
   return state;
