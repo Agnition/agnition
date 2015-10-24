970cfedddb62c14133293d3c06323b51fea0be3b
hdh3000 <henry.hutcheson@gmail.com>
2015-10-23 20:06:24 -0700

    added spec file for indvar wrapper

---
+++ specs/clientspecs/newexperiment/IndependentVar.spec.js
@@ -0,0 +1,39 @@
+//fake dom go first
+require('../index.js')();
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+
+var mocha = require('mocha');
+var expect = require('chai').expect;
+var mockRequire = require('mockrequire');
+var mockStore = require('../../utils/mockStore');
+var utils = require('../../utils/utils');
+
+var IndependentVarWrapper = mockRequire('../../../server/client/containers/newexperiment/IndependentVarWrapper',{
+  './IndependentVar' : utils.mockDivComponent('ind-var')
+}, {jsx:true});
+
+describe('the IndependentVarWrapper Container', function () {
+  var indVarWrapper;
+  beforeEach(function () {
+    var props = {};
+    var obj = {};
+    props.store = mockStore({});
+    props.expId = ['69'];
+    indVarWrapper = TestUtils.renderIntoDocument(React.createElement(IndependentVarWrapper, props), 'root');
+  });
+
+  it('should add a IndpendentVar on button press', function () {
+    var count = function() { 
+      return TestUtils.scryRenderedDOMComponentsWithClass(indVarWrapper, 'ind-var');
+    };
+    var  button = TestUtils.findRenderedDOMComponentWithTag(indVarWrapper,'button');
+    expect(count().length).to.eql(0);
+    TestUtils.Simulate.click(button);
+    expect(count().length).to.eql(1);
+  });
+
+
+});
+
