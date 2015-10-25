0c304740ee68811332d8ae6ad8b7495c99166bfa
hdh3000 <henry.hutcheson@gmail.com>
2015-10-24 17:23:49 -0700

    deleted console logs

--- server/client/reducers/Samples.js
+++ server/client/reducers/Samples.js
@@ -6,13 +6,9 @@ module.exports = function(state = initialState, action) {
     return Immutable.fromJS(action.samples);
   
   } else if (action.type === 'SET_IND_VAR_OPTION_ON_SAMPLE') {
-    //needs to be hooked up properly, action is fired..
-    var a = state.updateIn([action.sampleId, 'indVarStates'], function(map){
+    return state.updateIn([action.sampleId, 'indVarStates'], function(map){
       return map.set(action.indVarId, action.optionIndex);
     });
-    console.log('%c--> look here' , 'font-size:15px; padding-right:20px; color:white; background-color: black');﻿
-    console.dir(a.toJS);
-    return a;
 
   } else if (action.type === 'CREATE_SAMPLE') {
      return state.set(action.sampleId, new Immutable.Map({
