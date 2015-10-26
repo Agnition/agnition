f17e4b513f00aed3b8ed62934fb67e5a8a13c792
Marcus Buffett <marcusbuffett@me.com>
2015-10-26 11:12:11 -0700

    State -> un-normalized data

---
+++ server/client/utils/stateToNorm.js
@@ -0,0 +1,18 @@
+var _ = require('lodash');
+
+module.exports = function(state) {
+  var norm = {
+    entities: {},
+    result: {}
+  };
+  norm.entities.experiments = state.Experiments.toJS();
+  norm.entities.dependentVars = state.DepVars.toJS();
+  norm.entities.independentVars = state.IndVars.toJS();
+  norm.entities.measures = state.Measures.toJS();
+  norm.entities.reminders = state.Reminders.toJS();
+  norm.entities.samples = state.Samples.toJS();
+  norm.result.exps = _.map(norm.entities.experiments, function(_, expKey) {
+    return expKey;
+  });
+  return norm;
+};
