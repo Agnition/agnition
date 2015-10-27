68a56908ba4f4ad31013eb2f84a7d7d72cf98470
hdh3000 <henry.hutcheson@gmail.com>
2015-10-26 23:16:50 -0700

    deleted files

--- server/exps/modelComponents/freqValidator.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-//put this in its own seperate file becuase it is only validator shared by two schemas...
-module.exports = function (val) {
-  //accepts strings for now, 
-  //but in future will need to validate our recurring strings
-  return (typeof val === 'string' || val === null); 
-};
