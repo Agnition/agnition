3ceada8e3f59fdb48bcc4eff081aaec831d53147
Matthew Brooks <mdeanbrooks@gmail.com>
2015-10-27 19:06:21 -0700

    Prevent next/back button from going beyond range of questions + tests

--- server/client/reducers/NewExperiment.js
+++ server/client/reducers/NewExperiment.js
@@ -1,9 +1,17 @@
 module.exports = function(state = 0, action) {
   if (action.type === 'GO_TO_NEXT_QUESTION') {
-    return state + 1;
+    if(state < 5) {
+      return state + 1;
+    } else {
+      console.log('you are are at the last question already');
+    }
   }
   if (action.type === 'GO_TO_PREV_QUESTION') {
-    return state - 1;
+    if(state > 0) {
+      return state - 1;
+    } else {
+      console.log('You are at the first question already');
+    }
   }
   return state;
 };
