11641dab88cbc2adb9fe8c08ddb72d0e566dab11
Steven Lundy <steven.lundy@gmail.com>
2015-10-29 12:22:14 -0700

    Write util to get samples

--- server/client/utils/componentUtils.js
+++ server/client/utils/componentUtils.js
@@ -27,6 +27,30 @@ var divCollection = function(subElementobjects, subElementConstructor, propKey) 
 };
 
 
+var getSamplesForMeasure = function(state, measureId, indVarId) {
+  var measureKind = state.Measures.getIn([measureId, 'kind']);
+  var indVarName = state.IndVars.getIn([indVarId, 'name']);
+  var sampleIds = state.Measures.getIn([measureId, 'samples']).toJS();
+  var samples = _.map(sampleIds, function(sampleId) {
+    var sample = state.Samples.get(sampleId).toJS();
+    var indVarValue = _.first(_.pluck(_.filter(sample.indVars, function(indVar) {
+        //return indVar._id === this.props.indVarId; // TODO: use when id is consistent
+        return indVar.name === ownProps.name; // until db is consistent
+      }), 'value'))
+    return {
+      indVarValue: indVarValue,
+      measureValue: sample.value
+    };
+  });
+  return {
+    measureKind: measureKind,
+    indVarName: indVarName,
+    samples: samples
+  };
+};
+
+
 module.exports.mapIdsToObjs = mapIdsToObjs;
 module.exports.divCollection = divCollection;
+module.exports.getSamplesForMeasure = getSamplesForMeasure;
 
