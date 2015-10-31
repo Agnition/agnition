a29d3d5d3909f9fbefa657b6357fb3e61e704b0c
Steven Lundy <steven.lundy@gmail.com>
2015-10-30 21:25:59 -0700

    Write ANOVA functions

--- server/client/utils/ANOVA.js
+++ server/client/utils/ANOVA.js
@@ -1,14 +1,81 @@
-var SumOfSquaresWithinGroups = function(samples) {
-  var options = {}
-  for (var i = 0; i < samples.length; i++) {
-    samples[i]
+var criticalValues = require('./ANOVACriticalValues');
+
+var getOptionDegreesOfFreedom = function(data) {
+  return data.keys.length - 1;
+};
+
+var getSampleDegreesOfFreedom = function(data) {
+  var numOfOptions = 0;
+  var numOfSamples = 0;
+  for(var option in data) {
+    numOfOptions++;
+    numOfSamples += data[option].length;
   }
+  return numOfSamples - numOfOptions;
+};
+
+var getCriticalValue = function(sampleDegreesOfFreedom, optionDegreesOfFreedom) {
+  return criticalValues[sampleDegreesOfFreedom-1][optionDegreesOfFreedom-1];
+};
+
+var getMean = function(array) {
+  return array.reduce(function(a, b) {
+    return a + b;
+  }) / array.length;
+};
+
+var sumOfSquaresWithinGroups = function(data) {
+  var sumOfSquares = 0;
+  for (var option in data) {
+    var mean = getMean(data[option]);
+    var sumOfGroup = 0;
+    for (var i = 0; i < data[option].length; i++) {
+      sumOfGroup += Math.pow(mean - data[option][i], 2);
+    }
+    sumOfSquares += sumOfGroup;
+  }
+  return sumOfSquares;
+};
+
+var totalSumOfSquares = function(data) {
+  var fullData = [];
+  for (var option in data) {
+    fullData = fullData.concat(data[option]);
+  }
+  var mean = getMean(fullData);
+  var sumOfSquares = 0;
+  for (var i = 0; i < fullData.length; i++) {
+    sumOfSquares += Math.pow(mean - fullData[i], 2);
+  }
+  return sumOfSquares;
+};
+
+var sumOfSquaresBetweenGroups = function(data) {
+  var fullData = [];
+  for (var option in data) {
+    fullData = fullData.concat(data[option]);
+  }
+  var fullMean = getMean(fullData);
+  var sumOfSquares = 0;
+  for (var option in data) {
+    var mean = getMean(data[option]);
+    sumOfSquares += Math.pow(mean - fullMean, 2) * data[option].length;
+  }
+  return sumOfSquares;
+};
+
+var neglectNullHyposthesis = function(data) {
+  var sampleDegreeOfFreedom = getSampleDegreesOfFreedom(data);
+  var optionDegreeOfFreedom = getOptionDegreesOfFreedom(data);
+  var fRatio = (sumOfSquaresBetweenGroups(data) / optionDegreeOfFreedom) /
+               (sumOfSquaresWithinGroups(data) / sampleDegreeOfFreedom);
+  return fRatio > criticalValues(sampleDegreeOfFreedom, optionDegreeOfFreedom);
 };
 
 var getOptions = function(measureData) {
   var options = [];
   for (var i = 0; i < measureData.length; i++) {
-    if(options.indexOf(measureData[i].indVars.value) === -1) {
+    if (options.indexOf(measureData[i].indVars.value) === -1) {
       options.push(measureData[i].indVars.value);
     }
   }
@@ -28,11 +95,5 @@ var getValues = function(measureData) {
   }
 
   return data;
-};
-
-var getMean = function(array) {
-  return array.reduce(function(a, b) {
-    return a + b;
-  })/array.length;
 };
 
