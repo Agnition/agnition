5d060d94f4613f81d46e6ba9e74329c8a2e18a52
hdh3000 <henry.hutcheson@gmail.com>
2015-11-03 11:40:01 -0800

    progress wrapper now has correct styling

--- server/client/containers/NewExperiment/components/ProgressWrapper.js
+++ server/client/containers/NewExperiment/components/ProgressWrapper.js
@@ -7,24 +7,25 @@ var DepVars = require('../../../components/viewexperiment/DepVar');
 var IndVars = require('../../../components/viewexperiment/IndVar');
 
 function mapStatetoProps (state, ownProps) {
+  console.log("-------------------------------------------",ownProps);
   return {
     questionIndex: state.NewExperiment,
     depVarIds: state.Experiments.getIn([ownProps.expId, 'depVars']).toJS(),
-    indVarIds: state.Experiments.getIn([ownProps.expId, 'indVars']).toJS()
+    indVarIds: state.Experiments.getIn([ownProps.expId, 'indVars']).toJS(),
   };
 }
 
 var NewExperimentProgress = React.createClass({
 
   render: function() {
-  var progress = [(<ProgressName expId={this.props.expId} />),
+    var progress = [(<ProgressName expId={this.props.expId} />),
                   (<ProgressHypothesis expId={this.props.expId}/>),
                   (<DepVars depVarIds = {this.props.depVarIds}/>),
                   (<IndVars indVars = {this.props.indVarIds} />)
                 ];
     return (
-      <div className="col-md-6 col-sm-12">
-      <h2>Experiment Summary</h2>
+      <div className = 'section-inline'>
+      <h2 clasName = 'section-title'>Summary</h2>
       {progress.filter(function (component, i) {
         return i < this.props.questionIndex;
       }.bind(this))}
