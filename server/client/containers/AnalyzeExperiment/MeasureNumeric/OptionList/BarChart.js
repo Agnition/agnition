c6c9c05c2ab514ede169a9a26b246ad5671a2f48
hdh3000 <henry.hutcheson@gmail.com>
2015-11-06 15:20:39 -0800

    quick fixes

--- server/client/containers/AnalyzeExperiment/MeasureNumeric/OptionList/BarChart.js
+++ server/client/containers/AnalyzeExperiment/MeasureNumeric/OptionList/BarChart.js
@@ -12,9 +12,10 @@ var Chart = React.createClass({
         }];
     },
     render: function() {
+        console.dir(this.props.measure);
         return (
             <div className='chart-container-bar'>
-                <BarChart data={this.genChartData()} width={600} height={300} title={"Average " + this.props.indVar.name} yAxisLabel={this.props.measure.name} xAxisLabel={this.props.indVar.name} />
+                <BarChart data={this.genChartData()} width={600} height={300} title={"Average HEY"  + " @ " + this.props.indVar.name} yAxisLabel={this.props.measure.name} xAxisLabel={this.props.indVar.name} />
             </div>
         );
     }
