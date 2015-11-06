49657fd877876484d3e56c0562194c4bc8dec9ef
Steven Lundy <steven.lundy@gmail.com>
2015-11-06 15:04:02 -0800

    Add Help text to progress wrapper

--- server/client/containers/NewExperiment/components/ProgressWrapper.js
+++ server/client/containers/NewExperiment/components/ProgressWrapper.js
@@ -5,6 +5,7 @@ var ProgressName = require('./ProgressName');
 var ProgressHypothesis = require('./ProgressHypothesis');
 var DepVars = require('../../../components/viewexperiment/DepVar');
 var IndVars = require('../../../components/viewexperiment/IndVar');
+var HelpText = require('../../../components/HelpText.js');
 
 function mapStatetoProps (state, ownProps) {
   return {
@@ -19,8 +20,20 @@ var NewExperimentProgress = React.createClass({
   render: function() {
     var progress = [(<div><ProgressName expId={this.props.expId} /></div>),
                   (<div><ProgressHypothesis expId={this.props.expId}/></div>),
-                  (<div><h4 className="subsection-title-sm">Dependent Variable</h4><DepVars depVarIds = {this.props.depVarIds}/></div>),
-                  (<div><h4 className="subsection-title-sm">Independent Variable</h4><IndVars indVars = {this.props.indVarIds} /></div>)
+                  (<div>
+                    <h4 className="subsection-title-sm">
+                      Dependent Variable
+                      <HelpText message="This is the effect (i.e. what you are measuring)" />
+                    </h4>
+                    <DepVars depVarIds = {this.props.depVarIds}/>
+                  </div>),
+                  (<div>
+                    <h4 className="subsection-title-sm">
+                    Independent Variable
+                    <HelpText message="This is the cause (i.e. what you will change)" />
+                    </h4>
+                    <IndVars indVars = {this.props.indVarIds} />
+                  </div>)
                 ];
     return (
       <section className="subsection-v new-exp-section progress-section">
